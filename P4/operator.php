3ca3bea5bbc6b2c91012b2a4d1b91c0c886f5bc5
FirmanJK <jatikusuma761@gmail.com>
2024-09-21 06:05:36 +0700

    Soal No.2 dan 3

---
+++ P4/operator.php
@@ -0,0 +1,17 @@
+<?php
+$a = 10;
+$b = 5;
+
+$hasilTambah = $a + $b;
+$hasilKurang = $a - $b;
+$hasilKali = $a * $b;
+$hasilBagi = $a / $b;
+$sisaBagi = $a % $b;
+$pangkat = $a ** $b;
+
+echo "Hasil penjumlahan $a dan $b adalah $hasilTambah. <br>";
+echo "Hasil pengurangan $a dan $b adalah $hasilKurang. <br>";
+echo "Hasil perkalian $a dan $b adalah $hasilKali. <br>";
+echo "Hasil pembagian  $a dan $b adlaah $hasilBagi. <br>";
+echo "Sisa bagi $a dan $b adalah $sisaBagi. <br>";
+echo "Hasil pangkat $a dan $b adalah $pangkat. <br>";
