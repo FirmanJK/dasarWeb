c2987fc3ddddb918c6fa651f64036623e44bd048
FirmanJK <jatikusuma761@gmail.com>
2024-09-21 04:59:50 +0700

    Soal No.1

--- P4/variabel_konstanta.php
+++ P4/variabel_konstanta.php
@@ -2,11 +2,11 @@
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
-echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil. "
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil. ";
 
 $benar = true;
 $salah = false;
-echo "Variabel benar : $benar, variabel salah: $salah.";
+echo "Variabel benar : $benar, variabel salah : $salah.";
 
 // Mendifinisikan konstanta unutk nilai tetap
 define("NAMA_SITUS", "WebsiteKu.com");
