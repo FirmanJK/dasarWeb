44d0c910f96986b65d166c94959975f7f6bb409d
FirmanJK <jatikusuma761@gmail.com>
2024-09-25 16:19:51 +0700

    Soal 9-11

--- Praktik_php/P5/rekursif.php
+++ Praktik_php/P5/rekursif.php
@@ -1,9 +1,22 @@
 <?php
-function tampilkanHaloDunia() {
-    echo "Halo dunia! <br>";
-
-    tampilkanHaloDunia();
+// Fungsi untuk menampilkan pesan "Halo dunia!" sebanyak 25 kali
+function tampilkanPesan() {
+    for ($i = 1; $i <= 25; $i++) {
+        echo "Perulangan ke-{$i} <br>";
+    }
 }
 
-tampilkanHaloDunia();
+// Memanggil fungsi untuk menampilkan pesan
+tampilkanPesan();
+
+function tampilkanAngka(int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+
+    // Panggil diri sendiri selama $indeks <= $jumlah
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+
+tampilkanAngka(20);
 ?>
