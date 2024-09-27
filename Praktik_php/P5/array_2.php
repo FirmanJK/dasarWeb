f789d94862f7944d88ef599d85c44638bb2606f1
FirmanJK <jatikusuma761@gmail.com>
2024-09-28 06:02:12 +0700

    Perubahan 1-3

--- Praktik_php/P5/array_2.php
+++ Praktik_php/P5/array_2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tabel Data Dosen</title>
+    <title>Identitas Dosen</title>
  
     <link rel="stylesheet" href="style.css">
 </head>
