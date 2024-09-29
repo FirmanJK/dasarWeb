fbc93c51e530b3f7fccfd486d2b9c67f1c80e7e7
FirmanJK <jatikusuma761@gmail.com>
2024-09-30 06:37:58 +0700

    Soal No.11

--- Praktik_php/P5/menu.php
+++ Praktik_php/P5/menu.php
@@ -20,16 +20,9 @@ $menu = [
 
 function tampilkanMenuBertingkat(array $menu) {
     echo "<ul>";
-
     foreach ($menu as $item) {
         echo "<li>{$item['nama']}</li>";
-
-        // Jika item memiliki submenu, panggil fungsi ini secara rekursif
-        if (isset($item['subMenu'])) {
-            tampilkanMenuBertingkat($item['subMenu']);
-        }
     }
-
     echo "</ul>";
 }
 
