9e71fd4a9a133a8a841c135593d46c1175658bb0
FirmanJK <jatikusuma761@gmail.com>
2024-09-30 06:25:31 +0700

    Soal No.9

--- Praktik_php/P5/rekursif.php
+++ Praktik_php/P5/rekursif.php
@@ -5,18 +5,5 @@ function tampilkanHaloDunia() {
     tampilkanHaloDunia();
 
 }
-// Perulangan untuk menampilkan "Perulangan ke-i" sebanyak 25 kali
-for ($i=1; $i <= 25; $i++) {
-    echo "Perulangan ke-$i <br>";
-}
-function tampilkanAngka(int $jumlah, int $indeks = 1) {
-    echo "Perulangan ke-$indeks <br>";
-
-    // Panggil diri sendiri selama $indeks <= $jumlah
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);
-    }
-}
-
-tampilkanAngka(20);
+    tampilkanDunia();
 ?>
