5da493c4b98fa21153bee358084ec10e3dfdcdeb
FirmanJK <jatikusuma761@gmail.com>
2024-09-29 21:15:52 +0700

    No.4

--- Praktik_php/P5/fungsi.php
+++ Praktik_php/P5/fungsi.php
@@ -1,19 +1,11 @@
 <?php
-// Membuat fungsi untuk menghitung umur
-function hitungUmur($thn_lahir, $thn_sekarang) {
-    $umur = $thn_sekarang - $thn_lahir;
-    return $umur;
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Firman<br/>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
 }
 
-// Membuat fungsi untuk perkenalan
-function perkenalan($nama, $salam = "Assalamualaikum") {
-    echo "$salam, ";
-    echo "Perkenalkan, nama saya $nama<br>";
-    // Memanggil fungsi hitungUmur untuk menampilkan usia
-    echo "Saya berusia " . hitungUmur(1988, 2023) . " tahun<br>";
-    echo "Senang berkenalan dengan Anda<br>";
-}
-
-// Memanggil fungsi perkenalan
-perkenalan("Elok");
+// Memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
 ?>
