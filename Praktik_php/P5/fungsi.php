a47dc4dd9db8307bb707bca8d4e3b28c78ab5f0a
FirmanJK <jatikusuma761@gmail.com>
2024-09-30 06:12:43 +0700

    Saoal No.8

--- Praktik_php/P5/fungsi.php
+++ Praktik_php/P5/fungsi.php
@@ -1,11 +1,19 @@
 <?php
-
+//membuat fungsi untuk menghitung umur
 function hitungUmur($thn_lahir, $thn_sekarang) {
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
 
-// Memanggil fungsi hitungUmur dan mencetak hasilnya
-echo "Umur saya adalah ". hitungUmur(2004, 2024) . " tahun"; 
-// Ganti tahun lahir sesuai dengan Anda
+//membuat fungsi untuk perkenalan
+function perkenalan($nama, $salam="Assalamualaikum") {
+    echo "$salam, ";
+    echo "Perkenalkan, nama saya $nama<br/>";
+    // Memanggil fungsi hitungUmur di dalam fungsi perkenalan
+    echo "Saya berusia " . hitungUmur(2004, 2024) . " tahun<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+// Memanggil fungsi perkenalan
+perkenalan("Firman");
 ?>
