49ad14e4fdd56ff5b94b86951693f94b21dcc71f
FirmanJK <jatikusuma761@gmail.com>
2024-09-30 05:58:33 +0700

    Soal No.8

--- Praktik_php/P5/fungsi.php
+++ Praktik_php/P5/fungsi.php
@@ -1,19 +1,11 @@
 <?php
-//membuat fungsi
-function perkenalan($nama, $salam="Assalamualaikum") {
-    echo "$salam, ";
-    echo "Perkenalkan, nama saya $nama<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+// Membuat fungsi untuk menghitung umur
+function hitungUmur($thn_lahir, $thn_sekarang) {
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
 
-//memanggil fungsi yang sudah dibuat
-perkenalan("Hamdana", "Hallo");
-
-echo "<hr>";
-
-$saya = "Elok";
-$ucapanSalam = "Selamat pagi";
-
-//memanggil lagi tanpa mengisi parameter salam
-perkenalan($saya);
+// Memanggil fungsi hitungUmur dan mencetak hasilnya
+echo "Umur saya adalah ". hitungUmur(2004, 2024) . " tahun"; 
+// Ganti tahun lahir sesuai dengan Anda
 ?>
