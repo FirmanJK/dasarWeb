e607ac34dc1f6839c72fb26734dd94c30595324e
FirmanJK <jatikusuma761@gmail.com>
2024-09-30 05:53:41 +0700

    Perubahan No.6

--- Praktik_php/P5/fungsi.php
+++ Praktik_php/P5/fungsi.php
@@ -1,9 +1,9 @@
 <?php
 //membuat fungsi
 function perkenalan($nama, $salam="Assalamualaikum") {
-    echo $salam. ", ";
-    echo "Perkenalkan, nama saya ". $nama."<br>";
-    echo "Senang berkenalan dengan Anda<br>";
+    echo "$salam, ";
+    echo "Perkenalkan, nama saya $nama<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
 }
 
 //memanggil fungsi yang sudah dibuat
@@ -14,6 +14,6 @@ echo "<hr>";
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 
-//memanggil lagi
-perkenalan($saya, $ucapanSalam);
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan($saya);
 ?>
