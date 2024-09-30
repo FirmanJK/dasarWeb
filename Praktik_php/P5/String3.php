37f7411d1c9bf084a7dc3b41bddc3614a334a46e
FirmanJK <jatikusuma761@gmail.com>
2024-09-30 07:42:44 +0700

    Soal No.16

--- Praktik_php/P5/String3.php
+++ Praktik_php/P5/String3.php
@@ -1,4 +1,15 @@
 <?php
+
 $pesan = "Saya arek malang";
-echo strrev($pesan) . "<br>";
+
+# Ubah variabel $pesan menjadi array
+$pesanPerKata = explode(" ", $pesan);
+
+# Ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+
+# Gabungkan kembali array menjadi string
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan, "<br>";
 ?>
