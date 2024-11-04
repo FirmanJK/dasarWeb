c6e436083ec7ec27f0adf7cb0217c96b9e651fb7
FirmanJK <jatikusuma761@gmail.com>
2024-11-04 07:48:27 +0700

    perubahan upload

--- P8/upload.php
+++ P8/upload.php
@@ -1,13 +1,20 @@
 <?php
 if (isset($_POST["submit"])) {
 
-    $targetDirectory = "uploads/"; // Direktori tujuan untuk menyimpan file
+    $targetDirectory = "uploads/";
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxFileSize = 5 * 1024 * 1024;
 
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-        echo "File berhasil diunggah.";
+    if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
     } else {
-        echo "Gagal mengunggah file.";
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
 }
 ?>
