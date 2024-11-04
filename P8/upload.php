2c8109aea416cc3afa3818e05e2ad5196236a105
FirmanJK <jatikusuma761@gmail.com>
2024-11-04 19:31:34 +0700

    Praktikum 1

--- P8/upload.php
+++ P8/upload.php
@@ -1,15 +1,15 @@
 <?php
 if (isset($_POST["submit"])) {
-
-    $targetDirectory = "uploads/";
+    $targetDirectory = "uploads/"; // Direktori tujuan untuk menyimpan file
     $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
     $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxFileSize = 5 * 1024 * 1024;
 
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxFileSize = 10 * 1024 * 1024; 
+    
     if (in_array($fileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize) {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah. <br>";
         } else {
             echo "Gagal mengunggah file.";
         }
