79de7ff8e17e52e520fe653642172677d65e3600
FirmanJK <jatikusuma761@gmail.com>
2024-11-16 11:16:39 +0700

    P8-10

--- praktik_php/P9/koneksi.php
+++ praktik_php/P9/koneksi.php
@@ -1,8 +1,16 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = " ";
-    $dbname = "prakwebdb";
+<?php 
 
-    $connect = new mysqli($servername, $username, $password, $dbname);
+$serverName = "HPLAPTOP-14CM0"; //Nama server dapat dilihat di ssms.
+$connectionInfo = array( "Database"=>"TSQL2012");  
+  
+/* Connect using Windows Authentication. */  
+$conn = sqlsrv_connect( $serverName, $connectionInfo);  
+if( $conn === false )  
+{  
+     echo "Unable to connect.</br>";  
+     die( print_r( sqlsrv_errors(), true));
+} else {
+    echo "konek";
+}
+
 ?>
